48ebb810d90c27ae47dbfbf54ba7b2b132884234
thir13en <santilpzate@gmail.com>
2019-05-22 20:15:43 +0200

    adding first functional model

---
+++ src/shared/network/endpoints.ts
@@ -0,0 +1,3 @@
+export const API_ENDPOINTS = {
+    ALL_RESERVATIONS: 'reservations/read.php'
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
+  <div class='container'>
+    <div class='row'>
+      <div class='col-xs-12'>
         <h1>Listado de reservas</h1>
       </div>
     </div>
-    <div class="row d-none">
-      <div class="col-xs-12">
+    <div class='row d-none'>
+      <div class='col-xs-12'>
         <a>Próximas 24 horas</a>
       </div>
     </div>
-    <div class="row" id="reservationsTableContainer">
-      <div class="col-xs-12">
+    <div class='row' id='reservationsTableContainer'>
+      <div class='col-xs-12'>
         <h3>No hay reservas que mostrar</h3>
       </div>
     </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <button class="btn btn-primary">Crear reserva</button>
+    <div class='row'>
+      <div class='col-xs-12'>
+        <button class='btn btn-primary'>Crear reserva</button>
       </div>
     </div>
   </div>
@@ -25,12 +25,19 @@
 
 <script>
 // @ is an alias to /src
+import * as axios from 'axios';
 import ReservationList from '@/components/ReservationList.vue'
+import { API_ENDPOINTS } from '../shared/network/endpoints';
 
 export default {
   name: 'home',
   components: {
     ReservationList
+  },
+  created() {
+    axios
+        .get([process.env.VUE_APP_API_BASE_URL, API_ENDPOINTS.ALL_RESERVATIONS].join(''))
+        .then(res => console.log(res));
   }
 }
 </script>
