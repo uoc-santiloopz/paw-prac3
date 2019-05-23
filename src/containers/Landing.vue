f48c5baa441489095015c2ed192582e035d75a46
thir13en <santilpzate@gmail.com>
2019-05-23 19:28:10 +0200

    added reservations next 24 logic

--- src/containers/Landing.vue
+++ src/containers/Landing.vue
@@ -5,14 +5,14 @@
             <h1>Listado de reservas</h1>
           </div>
         </div>
-        <div class='row d-none'>
+        <div class='row' v-if="reservationsNext24 && reservationsNext24.length">
           <div class='col-xs-12'>
             <a>Próximas 24 horas</a>
           </div>
         </div>
         <div class='row' id='reservationsTableContainer'>
           <div class='col-xs-12'>
-            <h3 v-if="!reservations">No hay reservas que mostrar</h3>
+            <h3 v-if="!reservations || !reservations.length">No hay reservas que mostrar</h3>
             <ReservationList v-else :reservations="reservations"/>
           </div>
         </div>
@@ -35,6 +35,7 @@ export default {
     data() {
         return {
             reservations: null,
+            reservationsNext24: null,
             interval: null,
         }
     },
@@ -44,14 +45,24 @@ export default {
     methods: {
         fetchReservations() {
             axios
-                .get([process.env.VUE_APP_API_BASE_URL, API_ENDPOINTS.ALL_RESERVATIONS].join(''))
-                .then(res => this.reservations = res.data.map(res => new ReservationModel(res)));
+                .get([process.env.VUE_APP_API_BASE_URL, API_ENDPOINTS.RESERVATIONS_ALL].join(''))
+                .then(res => this.reservations =
+                    res.data && res.data.length ? res.data.map(res => new ReservationModel(res)) : []
+                );
+        },
+        fetchReservations24() {
+            axios
+                .get([process.env.VUE_APP_API_BASE_URL, API_ENDPOINTS.RESERVATIONS_24].join(''))
+                .then(res => this.reservationsNext24 =
+                    res.data && res.data.length ? res.data.map(res => new ReservationModel(res)) : []
+                );
         }
     },
     created() {
         this.interval = setInterval(() => {
-            this.fetchReservations();
+            this.fetchReservations24();
         }, 60000);
+        this.fetchReservations24();
         this.fetchReservations();
     },
     destroyed() {
--- src/shared/network/endpoints.ts
+++ src/shared/network/endpoints.ts
@@ -1,3 +1,8 @@
 export const API_ENDPOINTS = {
-    ALL_RESERVATIONS: 'reservations/read.php'
+    RESERVATIONS_ALL: 'reservations/read.php',
+    RESERVATIONS_24: 'reservations/read24.php',
+    RESERVATIONS_SINGLE: 'reservations/read_single.php?id=',
+    RESERVATIONS_CREATE: 'reservations/create.php',
+    RESERVATIONS_UPDATE: 'reservations/update.php?id=',
+    RESERVATIONS_DELETE: 'reservations/delete.php'
 };
